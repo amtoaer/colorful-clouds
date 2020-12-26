164d1264d1a7cb7fc3a4b93d8bbb1a97440ed09b
amtoaer <amtoaer@gmail.com>
2020-12-26 17:08:56 +0800

    feat: add str->image dict

--- helper/utils.py
+++ helper/utils.py
@@ -1,4 +1,6 @@
 import json
+import os
+import tkinter
 
 
 def read_config() -> dict:
@@ -14,3 +16,11 @@ def increase(current: int, len: int) -> int:
     城市号递增规则
     '''
     return (current+1) % len
+
+
+def get_images() -> dict:
+    result = dict()
+    for image in os.listdir('./img'):
+        result[image.split('.')[0]] = tkinter.PhotoImage(
+            file='./img/{}'.format(image))
+    return result
