c39b2698f6e6cce5e870c545e09282acdab4fa96
amtoaer <amtoaer@gmail.com>
2021-01-01 00:50:06 +0800

    feat: add next button, use a new thread to refresh weather data

--- ui/window.py
+++ ui/window.py
@@ -1,7 +1,9 @@
+from tkinter.constants import TOP
 from dao.data import Data
 from helper.utils import get_images
 import tkinter
 from tkinter import Button, Frame, Label
+import threading
 
 
 class Window(tkinter.Tk):
@@ -10,7 +12,7 @@ class Window(tkinter.Tk):
     def __init__(self):
         super().__init__()
         self.title('天气预报')
-        # self.geometry('800x600')
+        self.resizable(width=0, height=0)
         self.columnconfigure(0, weight=1)
         self.rowconfigure(0, weight=1)
         # Frame
@@ -20,7 +22,7 @@ class Window(tkinter.Tk):
 
         self.bottom_frame = Frame(self)
         # 按钮
-        self.help = Button(self.top_frame, text='帮助')
+        self.next = Button(self.top_frame, text='切换')
         self.setting = Button(self.top_frame, text='设置')
         self.refresh = Button(self.top_frame, text="刷新")
         # 数据源
@@ -29,26 +31,36 @@ class Window(tkinter.Tk):
         # Label
         self.current_weather_text = tkinter.StringVar()
         self.current_weather_label = Label(self.bottom_frame, image=None)
+        self.future_temperature_labels = [
+            tkinter.StringVar() for _ in range(7)]
         self.future_weather_labels = [
-            Label(self.bottom_frame, image=None) for _ in range(7)]
+            Label(self.bottom_frame, image=None, textvariable=self.future_temperature_labels[i], compound=TOP, width=80) for i in range(7)]
 
     def __draw(self):
         # Label充当占位符
         Label(self.top_frame).grid(row=0, column=0, columnspan=6, sticky='NS')
-        self.refresh.grid(row=0, column=7, sticky='NS', padx=20, pady=20)
-        self.setting.grid(row=0, column=8, sticky='NS', pady=20)
-        self.help.grid(row=0, column=9, sticky='NS', padx=20, pady=20)
+        self.next.grid(row=0, column=7, sticky='NS', padx=20, pady=20)
+        self.refresh.grid(row=0, column=8, sticky='NS', pady=20)
+        self.setting.grid(row=0, column=9, sticky='NS', padx=20, pady=20)
         self.current_weather_label.grid(
             row=0, column=0, rowspan=2, columnspan=7)
         Label(self.bottom_frame, textvariable=self.current_weather_text).grid(
-            row=0, column=8, columnspan=7)
+            row=0, column=8, columnspan=7, sticky='NWSE')
         for i in range(7):
             self.future_weather_labels[i].grid(row=1, column=7+i)
         self.top_frame.grid(row=0, column=0, sticky='WE')
         self.bottom_frame.grid(row=1, column=0, sticky='WE')
 
     def __bind_event(self):
-        self.refresh.configure(command=self.__refresh)
+        self.refresh.configure(command=self.__refresh_in_thread)
+        self.next.configure(command=self.__next)
+
+    def __next(self):
+        self.dao.increase()
+        self.__refresh_in_thread()
+
+    def __refresh_in_thread(self):
+        threading.Thread(target=self.__refresh).start()
 
     def __refresh(self):
         success = self.dao.refresh()
@@ -66,8 +78,11 @@ class Window(tkinter.Tk):
         风速：{}    pm2.5：{}
         空气质量：{}'''.format(current_data['city'], current_data['wea'], current_data['tem'], current_data['tem1'], current_data['tem2'], current_data['win'], current_data['win_speed'], current_data['air_pm25'], current_data['air_level']))
         for i in range(7):
+            data = future_data['data'][i]
+            self.future_temperature_labels[i].set(
+                '{}~{}'.format(data['tem2'], data['tem1']))
             self.future_weather_labels[i].configure(
-                image=self.images[future_data['data'][i]['wea_img']][1])
+                image=self.images[data['wea_img']][1])
 
     def __show(self):
         self.mainloop()
